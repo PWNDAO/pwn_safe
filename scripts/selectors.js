732c0cf39b16a4723749f68ff4f0dd8d6ddf1ae2
ashhanai <ashhab.naim@gmail.com>
2022-03-30 13:50:35 +0200

    update shared interfaces to include ERC20 allowance increase & decrease functions

--- scripts/selectors.js
+++ scripts/selectors.js
@@ -5,6 +5,10 @@ function selector(signature) {
 }
 
 console.log("------");
-console.log("ERC721-setApprovalForAll:\t", selector("setApprovalForAll(address,bool)"));
-console.log("ERC721-approve:\t\t", selector("approve(address,uint256)"));
+console.log("ERC20-approve:", selector("approve(address,uint256)")); // 0x095ea7b3
+console.log("ERC20-increaseAllowance:", selector("increaseAllowance(address,uint256)"));// 0x39509351
+console.log("ERC20-decreaseAllowance:", selector("decreaseAllowance(address,uint256)")); // 0xa457c2d7
+console.log("ERC721-approve:", selector("approve(address,uint256)")); // 0x095ea7b3
+console.log("ERC721-setApprovalForAll:", selector("setApprovalForAll(address,bool)")); // 0xa22cb465
+console.log("ERC1155-setApprovalForAll:", selector("setApprovalForAll(address,bool)")); // 0xa22cb465
 console.log("------");
