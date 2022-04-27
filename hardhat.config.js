c867429483823fe0e3c4abdfa077ccf4611eac7f
ashhanai <ashhab.naim@gmail.com>
2022-04-27 15:16:35 +0200

    Enable optimizer with 200 runs

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,10 @@ module.exports = {
   solidity: {
     version: "0.8.9",
     settings: {
-      // optimizer: {
-      //   enabled: true,
-      //   runs: 200,
-      // },
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
       outputSelection: {
         "*": {
           "*": ["storageLayout"]
