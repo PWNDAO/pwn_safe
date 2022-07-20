5e8b59a96c24ff65971411662f5653e1f73f548b
ashhanai <ashhab.naim@gmail.com>
2022-07-20 14:35:01 +0200

    Update deploy script

--- script/AssetTransferRights.s.sol
+++ script/AssetTransferRights.s.sol
@@ -5,13 +5,15 @@ import "forge-std/Script.sol";
 import "../src/AssetTransferRights.sol";
 
 
-/// Deploy AssetTransferRights contract by executing commands:
-///
-/// source .env
-/// forge script script/Deploy.s.sol \
-/// --rpc-url $RINKEBY_URL \
-/// --private-key $DEPLOY_PRIVATE_KEY_TESTNET \
-/// --broadcast
+/*
+Deploy AssetTransferRights contract by executing commands:
+
+source .env
+forge script script/AssetTransferRights.s.sol:Deploy \
+--rpc-url $RINKEBY_URL \
+--private-key $DEPLOY_PRIVATE_KEY_TESTNET \
+--broadcast
+ */
 
 contract Deploy is Script {
 
