8544f28d780264e09d4e3c112fe4373effee8558
ashhanai <ashhab.naim@gmail.com>
2023-01-04 12:38:51 +0100

    Update deploy scripts

--- script/PWNSafe.s.sol
+++ script/PWNSafe.s.sol
@@ -3,12 +3,13 @@ pragma solidity 0.8.15;
 
 import "forge-std/Script.sol";
 
-import "../src/factory/PWNSafeFactory.sol";
-import "../src/guard/AssetTransferRightsGuard.sol";
-import "../src/guard/AssetTransferRightsGuardProxy.sol";
-import "../src/guard/OperatorsContext.sol";
-import "../src/AssetTransferRights.sol";
-
+import "@pwn-safe/factory/PWNSafeFactory.sol";
+import "@pwn-safe/guard/AssetTransferRightsGuard.sol";
+import "@pwn-safe/guard/AssetTransferRightsGuardProxy.sol";
+import "@pwn-safe/guard/OperatorsContext.sol";
+import "@pwn-safe/handler/CompatibilityFallbackHandler.sol";
+import "@pwn-safe/module/AssetTransferRights.sol";
+import "@pwn-safe/Whitelist.sol";
 
 /*
 Deploy PWNSafe contracts by executing commands:
@@ -16,6 +17,7 @@ Deploy PWNSafe contracts by executing commands:
 source .env
 
 forge script script/PWNSafe.s.sol:Deploy \
+--sig "deploy(address,address,address)" $ADMIN $WHITELIST $HANDLER \
 --rpc-url $RPC_URL \
 --private-key $DEPLOY_PRIVATE_KEY \
 --with-gas-price $(cast --to-wei 10 gwei) \
@@ -24,58 +26,65 @@ forge script script/PWNSafe.s.sol:Deploy \
 */
 contract Deploy is Script {
 
-	function run() external {
+	function deployWhitelist() external {
 		vm.startBroadcast();
 
-		deploy(
-			address(0x0), // Fill admin
-			address(0x3E5c63644E683549055b9Be8653de26E0B4CD36E),
-			address(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2),
-			// address(0xF97779f08Fa2f952eFb12F5827Ad95cE26fEF432) // Goerli
-			address(0x90aE21bA079Fd8b3e8f29d0caDA21517D3e8dc8b) // Mumbai
-		);
+		Whitelist whitelist = new Whitelist();
+		console2.log("Whitelist address:", address(whitelist));
 
 		vm.stopBroadcast();
 	}
 
+	function deployFallbackHandler(address whitelist) external {
+		vm.startBroadcast();
+
+		CompatibilityFallbackHandler handler = new CompatibilityFallbackHandler(whitelist);
+		console2.log("CompatibilityFallbackHandler address:", address(handler));
+
+		vm.stopBroadcast();
+	}
 
 	function deploy(
 		address admin,
-		address gnosisSafeSingleton,
-		address gnosisSafeFactory,
+		address whitelist,
 		address gnosisFallbackHandler
-	) private {
-		// 1. Deploy ATR contract
-		AssetTransferRights atr = new AssetTransferRights();
+	) external {
+		vm.startBroadcast();
 
-		// 2. Deploy ATR Guard logic
+		address gnosisSafeSingleton = address(0x3E5c63644E683549055b9Be8653de26E0B4CD36E);
+		address gnosisSafeFactory = address(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
+
+		// 1. Deploy ATR contract
+		AssetTransferRights atr = new AssetTransferRights(whitelist);
+		console2.log("AssetTransferRights address:", address(atr));
+
+		// 2. Deploy ATR guard logic
 		AssetTransferRightsGuard guardLogic = new AssetTransferRightsGuard();
+		console2.log("AssetTransferRightsGuard address:", address(guardLogic));
 
 		// 3. Deploye ATR Guard proxy with ATR Guard logic
 		AssetTransferRightsGuardProxy guardProxy = new AssetTransferRightsGuardProxy(
 			address(guardLogic), admin
 		);
+		console2.log("AssetTransferRightsGuardProxy address:", address(guardProxy));
 
-		// 4. Deploy Operators Context
-		OperatorsContext operatorsContext = new OperatorsContext(address(guardProxy));
+		// 4. Initialized ATR Guard proxy as ATR Guard
+		AssetTransferRightsGuard(address(guardProxy)).initialize(address(atr));
 
-		// 5. Initialized ATR Guard proxy as ATR Guard
-		AssetTransferRightsGuard(address(guardProxy)).initialize(address(atr), address(operatorsContext));
-
-		// 6. Deploy PWNSafe factory
+		// 5. Deploy PWNSafe factory
 		PWNSafeFactory factory = new PWNSafeFactory(
 			gnosisSafeSingleton,
 			gnosisSafeFactory,
-			gnosisFallbackHandler,
+			address(gnosisFallbackHandler),
 			address(atr),
 			address(guardProxy)
 		);
+		console2.log("PWNSafeFactory address:", address(factory));
 
-		// 7. Set guard address to ATR contract
-		atr.setAssetTransferRightsGuard(address(guardProxy));
+		// 6. Initialize ATR contract
+		atr.initialize(address(factory), address(guardProxy));
 
-		// 8. Set PWNSafe validator to ATR contract
-		atr.setPWNSafeValidator(address(factory));
+		vm.stopBroadcast();
 	}
 
 }
