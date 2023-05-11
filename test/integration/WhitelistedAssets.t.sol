dce6c97a85d736cb6ca27d055c8997c5ab150b1c
ashhanai <ashhab.naim@gmail.com>
2023-05-11 14:16:40 +0200

    Test new whitelisted assets

--- test/integration/WhitelistedAssets.t.sol
+++ test/integration/WhitelistedAssets.t.sol
@@ -280,8 +280,12 @@ contract PolygonWhitelistedAssetsIntegrationTest is WhitelistedAssetsIntegration
     function test_PlaNFTs() external { _test_whitelistedAssets721(0xDBdb041842407c109F65b23eA86D99c1E0D94522); }
     function test_SyncSwapEraPioneer() external { _test_whitelistedAssets721(0x829C606D2ba4CDef61df2bBaC49718bD40024f02); }
     function test_ComethSpaceship() external { _test_whitelistedAssets721({ assetAddress: 0x85BC2E8Aaad5dBc347db49Ea45D95486279eD918, assetId: 7000686 }); }
+    function test_EmberSwordLand() external { _test_whitelistedAssets721(0xE7e16f2Da731265778f87cB8D7850E31b84b7b86); }
+    function test_Chumbi() external { _test_whitelistedAssets721(0x5492Ef6aEebA1A3896357359eF039a8B11621b45); }
 
     // 1155
     function test_PwnBundler() external { _test_whitelistedAssets1155({ assetAddress: 0xe52405604bF644349f57b36Ca6E85cf095faB8dA, assetId: 152, assetAmount: 1, assetOwner: 0x4cAd030bB05E59Fa23cCA62b417EEc22AEFF2f23 }); }
+    function test_PolkaPets() external { _test_whitelistedAssets1155({ assetAddress: 0xf0Bd260fcf279F3138726016B8a03c7110364E04, assetId: 1, assetAmount: 1, assetOwner: 0xC34aE1A39662415a4720d4A3e7C2Be0E202568C2 }); }
+    function test_ArkhanteBoosterPremiumPack() external { _test_whitelistedAssets1155({ assetAddress: 0x66d1bbf7Ad44491468465F56bf092F74ff84d6Ef, assetId: 630, assetAmount: 1, assetOwner: 0x000000000000000000000000000000000000dEaD }); }
 
 }
