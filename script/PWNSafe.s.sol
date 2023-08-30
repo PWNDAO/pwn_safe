f545090d75f228de834f0661724e12555b0ee52c
ashhanai <ashhab.naim@gmail.com>
2023-08-30 11:23:37 +0100

    Update deployment script

--- script/PWNSafe.s.sol
+++ script/PWNSafe.s.sol
@@ -43,10 +43,47 @@ library PWNDeployerSalt {
 }
 
 abstract contract PWNSafeScript is Script {
+    // Base, Cronos, Mantle
+    address constant internal PWN_DEPLOYER_OWNER = 0x1B4B37738De3bb9E6a7a4f99aFe4C145734c071d;
     address constant internal PWN_DEPLOYER = 0x706c9F2dd328E2C01483eCF705D2D9708F4aB727;
-    address constant internal GNOSIS_SAFE_SINGLETON = 0x3E5c63644E683549055b9Be8653de26E0B4CD36E;
-    address constant internal GNOSIS_SAFE_FACTORY = 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2;
-    address constant internal ADMIN = 0x61a77B19b7F4dB82222625D7a969698894d77473;
+    address constant internal GNOSIS_SAFE_SINGLETON = 0xfb1bffC9d739B8D520DaF37dF666da4C687191EA;
+    address constant internal GNOSIS_SAFE_FACTORY = 0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC;
+    address constant internal ADMIN = 0xa7106a1C2498EaeF4AC1B594a6544c841623B327;
+}
+
+interface GnosisSafeLike {
+    function execTransaction(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        uint8 operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver,
+        bytes memory signatures
+    ) external payable returns (bool success);
+}
+
+library GnosisSafeUtils {
+
+    function _gnosisSafeTx(GnosisSafeLike safe, address to, bytes memory data) internal returns (bool) {
+        uint256 ownerValue = uint256(uint160(msg.sender));
+        return GnosisSafeLike(safe).execTransaction({
+            to: to,
+            value: 0,
+            data: data,
+            operation: 0,
+            safeTxGas: 0,
+            baseGas: 0,
+            gasPrice: 0,
+            gasToken: address(0),
+            refundReceiver: payable(0),
+            signatures: abi.encodePacked(ownerValue, bytes32(0), uint8(1))
+        });
+    }
+
 }
 
 
@@ -56,35 +93,55 @@ Deploy PWNSafe contracts via EOA by executing commands:
 source .env
 
 forge script script/PWNSafe.s.sol:Deploy \
---sig "deploy(address)" $ADMIN \
 --rpc-url $RPC_URL \
 --private-key $PRIVATE_KEY \
 --with-gas-price $(cast --to-wei 10 gwei) \
 --verify --etherscan-api-key $ETHERSCAN_API_KEY \
 --broadcast
 
-forge verify-contract \
---chain-id X \
+forge verify-contract --watch --chain-id X \
 --constructor-args $(cast abi-encode "constructor()") \
-0x0 PWN.... $ETHERSCAN_API_KEY
+0x0 PWN.... -e $ETHERSCAN_API_KEY
 */
 contract Deploy is PWNSafeScript {
+    using GnosisSafeUtils for GnosisSafeLike;
 
-    function deploy(address admin) external {
+    function _deployAndTransferOwnership(bytes32 salt, address owner, bytes memory bytecode) internal returns (address) {
+        bool success = GnosisSafeLike(PWN_DEPLOYER_OWNER)._gnosisSafeTx({
+            to: PWN_DEPLOYER,
+            data: abi.encodeWithSelector(
+                IPWNDeployer.deployAndTransferOwnership.selector, salt, owner, bytecode
+            )
+        });
+        require(success, "Deploy failed");
+        return IPWNDeployer(PWN_DEPLOYER).computeAddress(salt, keccak256(bytecode));
+    }
+
+    function _deploy(bytes32 salt, bytes memory bytecode) internal returns (address) {
+        bool success = GnosisSafeLike(PWN_DEPLOYER_OWNER)._gnosisSafeTx({
+            to: PWN_DEPLOYER,
+            data: abi.encodeWithSelector(
+                IPWNDeployer.deploy.selector, salt, bytecode
+            )
+        });
+        require(success, "Deploy failed");
+        return IPWNDeployer(PWN_DEPLOYER).computeAddress(salt, keccak256(bytecode));
+    }
+
+
+    function run() external {
         vm.startBroadcast();
 
-        IPWNDeployer deployer = IPWNDeployer(PWN_DEPLOYER);
-
         // 1. Deploy Whitelist
-        address whitelist = deployer.deployAndTransferOwnership({
+        address whitelist = _deployAndTransferOwnership({
             salt: PWNDeployerSalt.WHITELIST,
-            owner: admin,
+            owner: ADMIN,
             bytecode: type(Whitelist).creationCode
         });
         console2.log("Whitelist address:", whitelist);
 
         // 2. Deploy Fallback handler
-        address handler = deployer.deploy({
+        address handler = _deploy({
             salt: PWNDeployerSalt.FALLBACK_HANDLER,
             bytecode: abi.encodePacked(
                 type(CompatibilityFallbackHandler).creationCode,
@@ -94,9 +151,9 @@ contract Deploy is PWNSafeScript {
         console2.log("CompatibilityFallbackHandler address:", handler);
 
         // 3. Deploy ATR contract
-        address atr = deployer.deployAndTransferOwnership({
+        address atr = _deployAndTransferOwnership({
             salt: PWNDeployerSalt.ATR,
-            owner: admin,
+            owner: ADMIN,
             bytecode: abi.encodePacked(
                 type(AssetTransferRights).creationCode,
                 abi.encode(whitelist)
@@ -105,18 +162,18 @@ contract Deploy is PWNSafeScript {
         console2.log("AssetTransferRights address:", atr);
 
         // 4. Deploy ATR guard logic
-        address guardLogic = deployer.deploy({
+        address guardLogic = _deploy({
             salt: PWNDeployerSalt.ATR_GUARD,
             bytecode: type(AssetTransferRightsGuard).creationCode
         });
         console2.log("AssetTransferRightsGuard address:", guardLogic);
 
         // 5. Deploye ATR Guard proxy with ATR Guard logic
-        address guardProxy = deployer.deploy({
+        address guardProxy = _deploy({
             salt: PWNDeployerSalt.ATR_GUARD_PROXY,
             bytecode: abi.encodePacked(
                 type(AssetTransferRightsGuardProxy).creationCode,
-                abi.encode(guardLogic, admin)
+                abi.encode(guardLogic, ADMIN)
             )
         });
         console2.log("AssetTransferRightsGuardProxy address:", guardProxy);
@@ -126,13 +183,11 @@ contract Deploy is PWNSafeScript {
         console2.log("ATR guard initialized");
 
         // 7. Deploy PWNSafe factory
-        address factory = deployer.deploy({
+        address factory = _deploy({
             salt: PWNDeployerSalt.FACTORY,
             bytecode: abi.encodePacked(
                 type(PWNSafeFactory).creationCode,
-                abi.encode(
-                    GNOSIS_SAFE_SINGLETON, GNOSIS_SAFE_FACTORY, handler, atr, guardProxy
-                )
+                abi.encode(GNOSIS_SAFE_SINGLETON, GNOSIS_SAFE_FACTORY, handler, atr, guardProxy)
             )
         });
         console2.log("PWNSafeFactory address:", factory);
@@ -140,130 +195,6 @@ contract Deploy is PWNSafeScript {
         // 8. Initialize ATR contract
         AssetTransferRights(atr).initialize(factory, guardProxy);
         console2.log("ATR module initialized");
-
-        vm.stopBroadcast();
-    }
-
-}
-
-
-//Use this functions if deploying via Gnosis Safe. Get deployment bytecode and pass it into tx builder.
-contract DeployBytecode is PWNSafeScript {
-
-    string SELECTED_FORK = "goerli";
-
-    function _deployBytecode(bytes32 salt, bytes memory bytecode) private returns (address, bytes memory) {
-        vm.createSelectFork(SELECTED_FORK);
-
-        bytes memory deployBytecode = abi.encodeWithSelector(
-            IPWNDeployer.deploy.selector, salt, bytecode
-        );
-
-        address addr = IPWNDeployer(PWN_DEPLOYER).computeAddress(salt, keccak256(bytecode));
-
-        return (addr, deployBytecode);
-    }
-
-    function _deployAndTransferOwnershipBytecode(bytes32 salt, address owner, bytes memory bytecode) private returns (address, bytes memory) {
-        vm.createSelectFork(SELECTED_FORK);
-
-        bytes memory deployBytecode = abi.encodeWithSelector(
-            IPWNDeployer.deployAndTransferOwnership.selector, salt, owner, bytecode
-        );
-
-        address addr = IPWNDeployer(PWN_DEPLOYER).computeAddress(salt, keccak256(bytecode));
-
-        return (addr, deployBytecode);
-    }
-
-
-    // forge script script/PWNSafe.s.sol:DeployBytecode --sig "whitelist()"
-    function whitelist() public returns (address, bytes memory) {
-        bytes memory bytecode = type(Whitelist).creationCode;
-        return _deployAndTransferOwnershipBytecode(PWNDeployerSalt.WHITELIST, ADMIN, bytecode);
-    }
-
-    // forge script script/PWNSafe.s.sol:DeployBytecode --sig "fallbackHandler()"
-    function fallbackHandler() public returns (address, bytes memory) {
-        (address _whitelist, ) = whitelist();
-
-        bytes memory bytecode = abi.encodePacked(
-            type(CompatibilityFallbackHandler).creationCode,
-            abi.encode(_whitelist)
-        );
-        return _deployBytecode(PWNDeployerSalt.FALLBACK_HANDLER, bytecode);
-    }
-
-    // forge script script/PWNSafe.s.sol:DeployBytecode --sig "atr()"
-    function atr() public returns (address, bytes memory) {
-        (address _whitelist, ) = whitelist();
-
-        bytes memory bytecode = abi.encodePacked(
-            type(AssetTransferRights).creationCode,
-            abi.encode(_whitelist)
-        );
-        return _deployAndTransferOwnershipBytecode(PWNDeployerSalt.ATR, ADMIN, bytecode);
-    }
-
-    // forge script script/PWNSafe.s.sol:DeployBytecode --sig "atrGuard()"
-    function atrGuard() public returns (address, bytes memory) {
-        bytes memory bytecode = type(AssetTransferRightsGuard).creationCode;
-        return _deployBytecode(PWNDeployerSalt.ATR_GUARD, bytecode);
-    }
-
-    // forge script script/PWNSafe.s.sol:DeployBytecode --sig "atrGuardProxy()"
-    function atrGuardProxy() public returns (address, bytes memory) {
-        (address guardLogic, ) = atrGuard();
-
-        bytes memory bytecode = abi.encodePacked(
-            type(AssetTransferRightsGuardProxy).creationCode,
-            abi.encode(guardLogic, ADMIN)
-        );
-        return _deployBytecode(PWNDeployerSalt.ATR_GUARD_PROXY, bytecode);
-    }
-
-    // Initialize guard proxy with atr + whitelist (cannot be done by ADMIN)
-
-    // forge script script/PWNSafe.s.sol:DeployBytecode --sig "factory()"
-    function factory() public returns (address, bytes memory) {
-        (address handler, ) = fallbackHandler();
-        (address _atr, ) = atr();
-        (address guardProxy, ) = atrGuardProxy();
-
-        bytes memory bytecode = abi.encodePacked(
-            type(PWNSafeFactory).creationCode,
-            abi.encode(GNOSIS_SAFE_SINGLETON, GNOSIS_SAFE_FACTORY, handler, _atr, guardProxy)
-        );
-        return _deployBytecode(PWNDeployerSalt.FACTORY, bytecode);
-    }
-
-    // Initialize ATR with factory + guard proxy
-
-}
-
-
-/*
-Set ATR token metadata URI by executing commands:
-
-source .env
-
-forge script script/PWNSafe.s.sol:Metadata \
---sig "set(address,string)" $ATR $METADATA_URI \
---rpc-url $RPC_URL \
---private-key $DEPLOY_PRIVATE_KEY \
---with-gas-price $(cast --to-wei 10 gwei) \
---broadcast
-*/
-contract Metadata is Script {
-
-    function set(
-        address atr,
-        string memory metadataUri
-    ) external {
-        vm.startBroadcast();
-
-        // Script have to be called by ATR contract owner
-        AssetTransferRights(atr).setMetadataUri(metadataUri);
 
         vm.stopBroadcast();
     }
